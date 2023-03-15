75ba79c3992cfb2f2a23199dab86fb61134cae2c
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-03-15 09:02:16 +0100

    App is working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {Name} from './components/Name';
+import {Food} from './components/Food';
+import {Drink} from './components/Drink';
+import {Result} from './components/Result';
 
 export const App = () => {
+  const [step, setStep] = useState(1);
+  const [name, setName] = useState('');
+  const [food, setFood] = useState('');
+  const [drink, setDrink] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleStepIncrease = () => {
+    setStep(step + 1)
+  };
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <>
+      {step === 1 && (
+        <Name name={name} setName={setName} />
+      )}
+      {step === 2 && (
+        <Food food={food} setFood={setFood} />
+      )}
+      {step === 3 && (
+        <Drink drink={drink} setDrink={setDrink} />
+      )}
+      {step === 4 && (
+        <Result name={name} food={food} drink={drink} />
+      )}
+      <>
+        <button type="button" onClick={handleStepIncrease}>Next step</button>
+        <p>Current step: {step}</p>
+      </>
+    </>
+  );
+};
